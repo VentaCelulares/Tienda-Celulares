6a7b807c29d3e41ecd547b3b3e7aa78d74ea07ea
crist <crist@DESKTOP-SOQJ11C.mshome.net>
2018-11-14 00:13:23 -0600

    implementasion del metodo salir

--- TiendaCelulares/src/mx/edu/uttt/FrontControler/FrontControlerAdministrador.java
+++ TiendaCelulares/src/mx/edu/uttt/FrontControler/FrontControlerAdministrador.java
@@ -4,11 +4,19 @@
  * and open the template in the editor.
  */
 package mx.edu.uttt.FrontControler;
+import javax.swing.JOptionPane;
+import mx.edu.uttt.dispacher.Dispatcher;
+import mx.edu.uttt.vistas.VistaLogin;
+import mx.edu.uttt.vistas.VistaMenuA;
+import mx.edu.uttt.vistas.VistaMenuV;
 
 /**
  *
  * @author crist
  */
 public class FrontControlerAdministrador {
-    
+    public void Salir(){
+    Dispatcher d=new Dispatcher();
+    d.dispatcher("salir");
+    }
 }
