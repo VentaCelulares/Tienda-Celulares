4ec6b89c1a6be5e45244ab1ad3a754cad0994de3
crist <crist@DESKTOP-SOQJ11C.mshome.net>
2018-11-14 00:20:24 -0600

    Creacion e codificacion del metodo de autenticar Usuario y contraseña

--- TiendaCelulares/src/mx/edu/uttt/FrontControler/FrontControlerLogin.java
+++ TiendaCelulares/src/mx/edu/uttt/FrontControler/FrontControlerLogin.java
@@ -4,11 +4,36 @@
  * and open the template in the editor.
  */
 package mx.edu.uttt.FrontControler;
-
+import javax.swing.JOptionPane;
+import mx.edu.uttt.dispacher.Dispatcher;
+import mx.edu.uttt.vistas.VistaLogin;
+import mx.edu.uttt.vistas.VistaMenuA;
+import mx.edu.uttt.vistas.VistaMenuV;
 /**
  *
  * @author crist
  */
 public class FrontControlerLogin {
-    
+     public boolean autenticUsuario(String user, String password){
+     boolean x=false;
+        if (user.equals("Admin")&&password.equals("admin")) {
+            Dispatcher d=new Dispatcher();
+            d.dispatcher("admin");
+           x=true;
+        }else if (user.equals("Public")&&password.equals("1234")) {
+            Dispatcher d=new Dispatcher();
+            d.dispatcher("public");
+                x=true;
+            }else if (user.equals("Public")|| user.equals("Admin") && (!password.equals("admin")|| !password.equals("1234")) ){
+            Dispatcher d=new Dispatcher();
+            d.dispatcher("Icon");
+            }else if (password.equals("admin")|| password.equals("1234") &&(!user.equals("Public")|| !user.equals("Admin"))){
+            Dispatcher d=new Dispatcher();
+            d.dispatcher("Ius");
+            }else{
+           Dispatcher d=new Dispatcher();
+            d.dispatcher("nlogin");
+            }
+        return x;
+        }
 }
