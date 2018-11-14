4714b05d7137de941bdd1ef7dbf0c90aa1ab7e49
crist <crist@DESKTOP-SOQJ11C.mshome.net>
2018-11-14 00:25:00 -0600

    implementasion de losmetodos de salir login vender

--- TiendaCelulares/src/mx/edu/uttt/FrontControler/FrontControlerMenuVenta.java
+++ TiendaCelulares/src/mx/edu/uttt/FrontControler/FrontControlerMenuVenta.java
@@ -4,11 +4,24 @@
  * and open the template in the editor.
  */
 package mx.edu.uttt.FrontControler;
-
+import mx.edu.uttt.dispacher.Dispatcher;
 /**
  *
  * @author crist
  */
 public class FrontControlerMenuVenta {
-    
+  
+    public void Salir(){
+    Dispatcher d=new Dispatcher();
+    d.dispatcher("salir");
+    }
+    public void login(){
+    Dispatcher d=new Dispatcher();
+    d.dispatcher("login");
+    }
+    public void vender(){
+    Dispatcher d=new Dispatcher();
+    d.dispatcher("vender");
+    }
+
 }
